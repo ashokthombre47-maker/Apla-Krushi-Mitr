cc32db592090c5c74f82d967843ba699ec850e99
Ashok Thombre <ashokthombre47@gmail.com>
2026-05-08 23:34:01 +0530

    Update LogoutServlet.java

--- src/main/java/controller/LogoutServlet.java
+++ src/main/java/controller/LogoutServlet.java
@@ -3,14 +3,27 @@ package controller;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 public class LogoutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
     throws ServletException, IOException {
 
-        HttpSession session = req.getSession();
-        session.invalidate();
+        // जर सेशन अस्तित्वात असेल तरच ते मिळवा, नवीन तयार करू नका (false)
+        HttpSession session = req.getSession(false);
 
-        res.sendRedirect("index.jsp");
+        if (session != null) {
+            // सेशन नष्ट करा
+            session.invalidate();
+        }
+
+        // लॉगिन पेजवर किंवा होमपेजवर रिडायरेक्ट करा
+        res.sendRedirect("login.jsp?msg=logout");
+    }
+
+    // सुरक्षिततेसाठी doPost मधून सुद्धा doGet ला कॉल करा
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+    throws ServletException, IOException {
+        doGet(req, res);
     }
 }
